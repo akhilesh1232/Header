f5ce776f1b1da939327bfbe17bec8378a9179d3c
Palak-shravagi <palakshravagi@gmail.com>
2021-07-14 11:54:58 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react'
 import './App.css'
 import './index.css';
 import Header from "./Components/Header";
-import { BrowserRouter, Switch, Route , Redirect, Link } from '../node_modules/react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect, Link } from '../node_modules/react-router-dom';
 
 export default class App extends React.Component {
-  render(){
-    return(<>
-      <div className = "auth-wrapper">
-      <div className="auth-inner">
+  render() {
+    return (<>
+      <div className="auth-wrapper">
+        <div className="auth-inner">
           <Switch>
-            <Route exact path = "/" component = {Header} />
+            <Route exact path="/" component={Header} />
             {/* <Route exact path = "/Home" component = {Home} />
             <Route exact path = "/Events" component = {Events} />
             <Route exact path = "/ClubService" component = {ClubService} />
@@ -19,9 +19,9 @@ export default class App extends React.Component {
             <Route exact path = "/Gallery" component = {} />
             <Route exact path = "/Login" component = {} />
             <Route exact path = "/ContactUs" component = {} /> */}
-            <Redirect to = "/" />
+            <Redirect to="/" />
           </Switch>
-      </div>
+        </div>
 
       </div>
     </>)
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,26 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import '../CSS/Header.css';
 import { NavLink } from 'react-router-dom';
 import Logo from '../output-onlinepngtools4.PNG';
 
-export default class Header extends Component
-{
-    render(){
-        return(
-        <>
-            <nav className="navbar navbar-expand-lg navbar-dark">
-                        <NavLink className="navbar-brand mx-3" to="/" >
-                            <img src = {Logo} alt = "Wlug Logo" height = "50px"  width = "100px" />
-                        </NavLink>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+export default class Header extends Component {
+    render() {
+        return (
+            <>
+                <nav className="navbar navbar-expand-lg navbar-dark">
+                    <NavLink className="navbar-brand mx-3" to="/" >
+                        <img src={Logo} alt="WLUG logo" height="50px" width="100px" />
+                    </NavLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{ disply: "flex", justifyItems: "center", justifyContent: "flex-end" }}>
                         <ul className="navbar-nav ml-auto mb-2 mb-lg-0">
                             <li className="nav-item px-3">
-                                <NavLink className="nav-link active" aria-current="page" to = "/">Home</NavLink>
+                                <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
                             </li>
                             <li className="nav-item px-3">
                                 <NavLink className="nav-link" to="/">Events</NavLink>
@@ -33,27 +32,27 @@ export default class Header extends Component
                             </li>
                             <li className="nav-item dropdown px-3">
                                 <NavLink className="nav-link dropdown-toggle" to="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Members
+                                    Members
                                 </NavLink>
                                 <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li className = "py-1"><NavLink className="dropdown-item" style = {{ backgroundColor: 'white', color: 'black' }}  to="/">Assistant Board</NavLink></li>
-                                    <li className = "py-1"><NavLink className="dropdown-item" style = {{ backgroundColor: 'white', color: 'black' }}  to="/">Member Board</NavLink></li>
-                                    <li className = "py-1"><NavLink className="dropdown-item" style = {{ backgroundColor: 'white', color: 'black' }} to="/">Mentor Board</NavLink></li>
+                                    <li className="py-1"><NavLink className="dropdown-item" style={{ backgroundColor: 'white', color: 'black' }} to="/">Member Board</NavLink></li>
+                                    <li className="py-1"><NavLink className="dropdown-item" style={{ backgroundColor: 'white', color: 'black' }} to="/">Main Board</NavLink></li>
+                                    <li className="py-1"><NavLink className="dropdown-item" style={{ backgroundColor: 'white', color: 'black' }} to="/">Mentor Board</NavLink></li>
                                 </ul>
                             </li>
                             <li className="nav-item px-3">
-                                <NavLink className="nav-link" to = "/">Alumni Portal</NavLink>
+                                <NavLink className="nav-link" to="/">Alumni Portal</NavLink>
                             </li>
                             <li className="nav-item px-3">
-                                <NavLink className="nav-link" to = "/">Gallery</NavLink>
+                                <NavLink className="nav-link" to="/">Gallery</NavLink>
                             </li>
                             <li className="nav-item px-3">
-                                <NavLink className="nav-link" to = "/">Contact Us</NavLink>
+                                <NavLink className="nav-link" to="/">Contact Us</NavLink>
                             </li>
                         </ul>
-                        </div>
-                    </nav>
-        </>
+                    </div>
+                </nav>
+            </>
         );
     }
 }
